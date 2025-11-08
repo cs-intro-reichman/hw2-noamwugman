00707fef134f035826617600edfa42b69b57f981
noamwugman <noamwug24@gmail.com>
2025-11-08 23:50:33 +0200

    noam

--- CalcPi.java
+++ CalcPi.java
@@ -5,11 +5,11 @@ public class CalcPi {
 		int counter = 0;
 		double piCalc = 1;
 		double deno = 1; // holds the last denominator (מכנה) that was added/subtracted
-		while (counter <= i) {
+		while (counter < i) {
 			if ((counter) % 2 == 0) {
-				piCalc = piCalc + (1 / (deno + 2));
+				piCalc = piCalc - (1 / (deno + 2));
 			}
-			else piCalc = piCalc - (1 / (deno + 2));
+			else piCalc = piCalc + (1 / (deno + 2));
 			deno = deno + 2;
 			counter++;
 		}
